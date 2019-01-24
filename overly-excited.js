b5306ebcd92f4f893aa34bf73b489386b26af0f6
sydneywait <sydneywait@gmail.com>
2019-01-24 09:27:05 -0500

    added option to tell how many times special character is added

--- overly-excited.js
+++ overly-excited.js
@@ -1,7 +1,7 @@
 console.log("********************practice one, Dancing Walruses")
 
 // Create an array that contains the words in the sentence
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
 /*
     The addExcitement function should be an impure function, and accept
@@ -9,7 +9,7 @@ let sentence = ["The","walrus","danced","through","the","trees","in","the","ligh
     and output the words to the browser console.
 */
 //add option for user to change the punctuation
-function addExcitement (theWordArray, punctuation) {
+function addExcitement(theWordArray, punctuation, number) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
@@ -17,22 +17,27 @@ function addExcitement (theWordArray, punctuation) {
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
         // buildMeUp = `${buildMeUp} ${sentence[i]}`
-  
-    // If the current value of `i` divided by 3 has no
-    // remainder, add an exclamation point to the end of
-    // the word and then concatenate it to `buildMeUp`.
 
-    
-    if((i + 1) % 3 === 0){
-        buildMeUp = `${buildMeUp} ${sentence[i]}${punctuation}`
-    }
-    // Otherwise, just concatenate the word itself.
-    else{
-        buildMeUp = `${buildMeUp} ${sentence[i]}`
-    }
+        // If the current value of `i` divided by 3 has no
+        // remainder, add an exclamation point to the end of
+        // the word and then concatenate it to `buildMeUp`.
 
-    
-    
+
+        if ((i + 1) % 3 === 0) {
+
+            buildMeUp = `${buildMeUp} ${sentence[i]}`
+
+            for (j = 0; j < number; j++) {
+                buildMeUp = `${buildMeUp}${punctuation}`
+            }
+        }
+        // Otherwise, just concatenate the word itself.
+        else {
+            buildMeUp = `${buildMeUp} ${sentence[i]}`
+        }
+
+
+
 
         // Print buildMeUp to the console
         console.log(buildMeUp);
@@ -41,7 +46,6 @@ function addExcitement (theWordArray, punctuation) {
 }
 
 // Invoke the function and pass in the array.  User should add which punctuation or character they want to see 
-addExcitement(sentence, "?")
+addExcitement(sentence, "*", 5)
 
 
-console.log("***************Practice two-")
